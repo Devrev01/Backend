4252cf981e46a0a6d8f61db6b6083e9823929e4e
gaurav3278 <gauravrai54152@gmail.com>
2023-09-24 19:20:36 +0530

    logout done

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,4 @@
 export const checkAuth = async(req,res,next) =>{
-    console.log(req.session)
     if(req.session.isAuthenicated && req.session.user){
         return res.status(200).json({isAuthenicated:true,user:req.session.user})
     }
