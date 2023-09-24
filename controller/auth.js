2a5874967574a1e29be217099ad26a21b9f06d60
gaurav3278 <gauravrai54152@gmail.com>
2023-09-24 21:59:10 +0530

    final final final

--- controller/auth.js
+++ controller/auth.js
@@ -66,7 +66,7 @@ export const googleLogin = async (req, res, next) => {
 
 export const googleCallback = async (req, res, next) => {
     console.log("google callback")
-    passport.authenticate("google", { failureRedirect: "http://localhost:3000/signin?error=emailNotFound", successRedirect: "http://localhost:3000/signin?success=loginSuccess" })(req,res,next)
+    passport.authenticate("google", { failureRedirect: "https://bookmanager2023.onrender.com/signin?error=emailNotFound", successRedirect: "https://bookmanager2023.onrender.com/signin?success=loginSuccess" })(req,res,next)
 }
 
 export const verifyEmail = async (req, res, next) => {
