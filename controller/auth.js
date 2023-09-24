e3e5f453ed284a0f83c977198ef473e3281b1183
gaurav3278 <gauravrai54152@gmail.com>
2023-09-24 22:24:55 +0530

    last last

--- controller/auth.js
+++ controller/auth.js
@@ -65,13 +65,7 @@ export const googleLogin = async (req, res, next) => {
 
 
 export const googleCallback = async (req, res, next) => {
-    passport.authenticate("google", { failureRedirect: "https://bookmanager2023.onrender.com/signin?error=emailNotFound"})(req,res,next),async(req,res,next)=>{
-        req.session.user = req.user
-        req.session.isAuthenicated = true
-        await req.session.save()
-        console.log("google callback")
-        res.redirect("https://bookmanager2023.onrender.com/signin?success=loginSuccess")
-    }
+    
 }
 
 export const verifyEmail = async (req, res, next) => {
--- route/auth.js
+++ route/auth.js
@@ -1,6 +1,8 @@
 import express from "express"
 import { login, register, logout, googleLogin, googleCallback, verifyEmail, verifyOtp } from "../controller/auth.js"
 import { checkAuth } from "../middleware/auth.js"
+import passport from "passport"
+import "../passport.js"
 
 const router = express.Router()
 router.post("/login", login)
@@ -9,7 +11,17 @@ router.post("/verifyOtp", verifyOtp)
 router.get("/check-auth", checkAuth)
 router.get("/verifyEmail", verifyEmail)
 router.get("/logout", logout)
-router.get('/google',googleLogin)
-router.get("/google/callback",googleCallback );
+router.get('/google', googleLogin)
+router.get('/google/callback',
+    passport.authenticate("google", { failureRedirect: "https://bookmanager2023.onrender.com/signin?error=emailNotFound" }),
+    (req, res, next) => {
+        req.session.user = req.user
+        req.session.isAuthenicated = true
+        req.session.save()
+        console.log("google callback")
+        res.redirect("https://bookmanager2023.onrender.com/signin?success=loginSuccess")
+    }
+)
+router.get("/google/callback", googleCallback);
 
 export default router
